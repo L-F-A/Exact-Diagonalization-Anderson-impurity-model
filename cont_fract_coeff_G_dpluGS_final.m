080cfe35a0933ea81fd9bcec383eca5897cba69e
Louis-Francois Arsenault <lf.arsenault@gmail.com>
2017-04-13 15:16:40 -0400

    Update cont_fract_coeff_G_dpluGS_final.m

--- cont_fract_coeff_G_dpluGS_final.m
+++ cont_fract_coeff_G_dpluGS_final.m
@@ -1,7 +1,5 @@
 function [an,bn2,ddplus] = cont_fract_coeff_G_dpluGS_final(Psi_GS,NSz_GS,C_ind,table,H_non_zero_ele,ed,U,ee,V,Ns)
 
- %FF = cell(1,size(NSz_GS,1));
- %nbr_coeff_cont = 400;
  nbr_coeff_cont = 800;
  for r_deg = 1:size(NSz_GS,1)
     Psi_GS_vec = Psi_GS{1,r_deg}; 
@@ -10,33 +8,19 @@ function [an,bn2,ddplus] = cont_fract_coeff_G_dpluGS_final(Psi_GS,NSz_GS,C_ind,t
     
     
     %We isolate in C the states that are part of the GS
-    %C_GS_inter = C{1,N_elec_GS+1};
     C_GS_inter = C_ind{1,N_elec_GS+1};
     
-    %indice_GS = find(C_GS_inter(:,2*Ns+1) == Sz_GS);
     indice_GS = find(C_GS_inter(:,2) == Sz_GS);
     C_GS = C_GS_inter(indice_GS,:);
     
-    %indice_f0 = find(C_GS(:,1) == 0);
     indice_f0 = find(bitget(C_GS(:,1),1) == 0);
     
-    
-%     if length(indice_f0) == 0
-%         an(r_deg,1) = 0;
-%         bn2(r_deg,1) = 0;
-%         ddplus(r_deg) = 0;
-%         fprintf('Size of Lanczos basis for sector d^dagger_up[%d\t%d]=[%d\t%d]: %d\n\n',N_elec_GS,Sz_GS,N_elec_GS+1,Sz_GS+1,length(indice_Lanczos_basis))
-%         
-%     else
     f01 = Psi_GS_vec(indice_f0,1);
     C_f0 = C_GS(indice_f0,:);
     C_f0_bin = bitset(table(C_f0(:,1)+1,4),2*Ns,1);
     C_f0(:,1) = table(C_f0_bin+1,5);
-    %C_f0(:,1) = 0;
     C_f0(:,2) = C_f0(:,2) + 1;
     
-    
-    %C_f0_sans_spin = C_f0(:,1:2*Ns);
     
     C_Lanczos = C_ind{1,N_elec_GS+2};
     indice_Lanczos_basis = find(C_ind{1,N_elec_GS+2}(:,2)==(Sz_GS+1));
@@ -53,18 +37,6 @@ function [an,bn2,ddplus] = cont_fract_coeff_G_dpluGS_final(Psi_GS,NSz_GS,C_ind,t
     
     [lig_f0,col_f0] = size(C_f0);
     [lig_Lan,col_Lan] = size(C_Lanczos);
-    
-%     for r = 1:2*Ns
-%         II(r) = mod(r,2)*2^((r+1)/2-1)+mod(r+1,2)*2^(Ns+r/2-1);
-%     end
-%     
-%     for r = 1:lig_Lan
-%         state_ind_Lan(r,1) = C_Lanczos(r,1:2*Ns)*II';
-%     end
-% 
-%     for r = 1:lig_f0
-%         state_ind_f0(r,1) = C_f0(r,1:2*Ns)*II';
-%     end
 
     state_ind_Lan = C_Lanczos(:,1);
     state_ind_f0 = C_f0(:,1);
@@ -120,13 +92,7 @@ function [an,bn2,ddplus] = cont_fract_coeff_G_dpluGS_final(Psi_GS,NSz_GS,C_ind,t
                     H = diag_H;
                 end
     
-    
-%     for ligne = 1:length(indice_Lanczos_basis)
-%       for colonne = 1:length(indice_Lanczos_basis)
-%          H(ligne,colonne) = Mat_element_AIM(C_Lanczos(ligne,:),C_Lanczos(colonne,:),Ns,ed,U,ee,V);
-%       end
-%     end
-    
+        
         f0_s_f0 = f0'*f0;
         ddplus(r_deg) = f0_s_f0;
         f0 = f0./sqrt(f0_s_f0);
@@ -145,8 +111,6 @@ function [an,bn2,ddplus] = cont_fract_coeff_G_dpluGS_final(Psi_GS,NSz_GS,C_ind,t
         f1 = f1./sqrt(b12);
         a1 = (f1'*H*f1);
     
-%         disp('<n|n-1>')
-%         f1'*f0
     
         an(r_deg,1) = a0;
         an(r_deg,2) = a1;
@@ -165,8 +129,6 @@ function [an,bn2,ddplus] = cont_fract_coeff_G_dpluGS_final(Psi_GS,NSz_GS,C_ind,t
       
         an(r_deg,pos) = a;
         bn2(r_deg,pos) = b;
-%         disp('<n|n-1>')
-%         f'*f1
         pos = pos+1;
         f0 = f1;
         f1 = f;
