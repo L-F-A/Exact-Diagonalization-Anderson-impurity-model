35a89f110b2df120bb774e1ab98be2c90d577544
Louis-Francois Arsenault <lf.arsenault@gmail.com>
2017-04-13 15:13:13 -0400

    Update Get_nd_D_final.m

--- Get_nd_D_final.m
+++ Get_nd_D_final.m
@@ -8,33 +8,18 @@ function [nd,ndup,nddown,nc,ncup,ncdown,D] = Get_nd_D_final(Psi_GS,NSz_GS,C_ind,
     
     
     %We isolate in C the states that are part of the GS
-    %C_GS_inter = C{1,N_elec_GS+1};
     C_GS_inter = C_ind{1,N_elec_GS+1};
     
-    %indice_GS = find(C_GS_inter(:,2*Ns+1) == Sz_GS);
     indice_GS = find(C_GS_inter(:,2) == Sz_GS);
     C_GS = C_GS_inter(indice_GS,:);
-    
-%     Sz_N = C_ind{1,N_elec_GS+1}(:,2);
-%     change_spin = find(diff(Sz_N)~=0)';
-%     nbr_change = length(change_spin);
-%     if nbr_change ~=0
-%       hh = [change_spin(1:length(change_spin)) change_spin(length(change_spin))+1];
-%       pos_spin_space = find(Sz_N(hh) == Sz_GS+1);
-%     end
-    
+        
     [lig_GS,col_GS] = size(C_GS);
     state_ind_GS = C_GS(:,1);
     
     nd_states_up = bitget(table(state_ind_GS(:,1)+1,4),2*Ns);
     nd_states_down = bitget(table(state_ind_GS(:,1)+1,4),2*Ns-1);
     nd_states = nd_states_up + nd_states_down;
-    %nd_states = bitget(table(state_ind_GS(:,1)+1,4),2*Ns)+bitget(table(state_ind_GS(:,1)+1,4),2*Ns-1);
     D_states = bitget(table(state_ind_GS(:,1)+1,4),2*Ns).*bitget(table(state_ind_GS(:,1)+1,4),2*Ns-1);
-%     nd(r_deg) = Psi_GS(:,r_deg)'*(nd_states.*Psi_GS(:,r_deg));
-%     ndup(r_deg) = Psi_GS(:,r_deg)'*(nd_states_up.*Psi_GS(:,r_deg));
-%     nddown(r_deg) = Psi_GS(:,r_deg)'*(nd_states_down.*Psi_GS(:,r_deg));
-%     D(r_deg) = Psi_GS(:,r_deg)'*(D_states.*Psi_GS(:,r_deg));
     nd(r_deg) = Psi_GS_vec'*(nd_states.*Psi_GS_vec);
     ndup(r_deg) = Psi_GS_vec'*(nd_states_up.*Psi_GS_vec);
     nddown(r_deg) = Psi_GS_vec'*(nd_states_down.*Psi_GS_vec);
@@ -48,10 +33,6 @@ function [nd,ndup,nddown,nc,ncup,ncdown,D] = Get_nd_D_final(Psi_GS,NSz_GS,C_ind,
        nc_states_up(:,oo) = bitget(table(state_ind_GS(:,1)+1,4),2*Ns-2*oo);
        nc_states_down(:,oo) = bitget(table(state_ind_GS(:,1)+1,4),2*Ns-2*oo-1);
        nc_states(:,oo) = nc_states_up(:,oo)+nc_states_down(:,oo);
-       %nc_states(:,oo) = bitget(table(state_ind_GS(:,1)+1,4),2*Ns-2*oo)+bitget(table(state_ind_GS(:,1)+1,4),2*Ns-2*oo-1);
-%        nc_inter(oo) = Psi_GS(:,r_deg)'*(nc_states(:,oo).*Psi_GS(:,r_deg));
-%        nc_inter_up(oo) = Psi_GS(:,r_deg)'*(nc_states_up(:,oo).*Psi_GS(:,r_deg));
-%        nc_inter_down(oo) = Psi_GS(:,r_deg)'*(nc_states_down(:,oo).*Psi_GS(:,r_deg));
        nc_inter(oo) = Psi_GS_vec'*(nc_states(:,oo).*Psi_GS_vec);
        nc_inter_up(oo) = Psi_GS_vec'*(nc_states_up(:,oo).*Psi_GS_vec);
        nc_inter_down(oo) = Psi_GS_vec'*(nc_states_down(:,oo).*Psi_GS_vec);
